9462cbe6d8c32bdfc6e4c119139b21f89e12bde0
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-06-07 13:49:02 +0900

    Update git_q_1.sh

--- git_q_1.sh
+++ git_q_1.sh
@@ -3,4 +3,5 @@ git stash clear
 curl https://raw.githubusercontent.com/usagrammer/git-exam/curl_raw_files/app/models/message.rb >> app/models/message.rb
 git add .
 git stash save "!!GitHub_Desktop<`git rev-parse --abbrev-ref HEAD`>"
+git checkout -b 'A_branch_'`date +"%Y年%m月%d日%H時%M分%S秒"`
 killall "GitHub Desktop"
